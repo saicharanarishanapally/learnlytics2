783093297435ae43d41d785705a2c634fae825b1
saicharanarishanapally <34207825+saicharanarishanapally@users.noreply.github.com>
2021-05-09 15:19:06 +0530

    Update site.js

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,9 +1,9 @@
 module.exports = {
-  title: 'Vredeburg',
-  description: 'Eleventy starter project to create a blog',
-  keywords: ['eleventy', 'template', 'simple', 'clean'],
+  title: 'Learnlytics',
+  description: 'A blog for for Data analysts, data scientists and AI engineers ',
+  keywords: ['data science','data analytics','machine learning','deep learning'],
   // your site url without trailing slash
-  url: 'https://vredeburg.netlify.app',
+  url: 'https://learnlytics.net',
   // how many posts you want to show for each page
   paginate: 6
   // if you want to add disqus to your site
